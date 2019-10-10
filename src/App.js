068bfb9f63f37ac3ad6771486b4080d5602a3d4c
ReallyLiri <reallyliri@gmail.com>
2019-10-10 15:10:30 +0300

    its a start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Lazy from 'react-lazyload'
+import Loader from 'react-loader-spinner'
+
+const Image = ({height, src}) => (
+  <Lazy height={height} placeholder={<Loader type="Rings" color="red" height={80} width={80}/>}>
+    <img style={{animation: `spin 3s linear infinite`}} height={height} src={src} alt="alt"/>
+  </Lazy>
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Image height={200} src='https://i.imgur.com/sUBPiUJ.png'/>
+        <Image height={200} src='https://i.imgur.com/QnqSVWm.png'/>
+        <Image height={200} src='https://i.imgur.com/E4xGjmt.png'/>
       </header>
     </div>
   );
 }
+
 
 export default App;
